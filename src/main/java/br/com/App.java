e95c2fec32df39d120a69240de839587b2bce77b
Tiago Sant'Ana <tiago.santana@gmail.com>
2017-10-31 19:12:54 -0200

    teste

--- src/main/java/br/com/App.java
+++ src/main/java/br/com/App.java
@@ -1,10 +1,7 @@
 package br.com;
 
-<<<<<<< HEAD
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-=======
->>>>>>> ffb687d311081f09790cf4316d6afd4fc9bc4b94
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -19,11 +16,9 @@ public class App {
 		Carro carro = (Carro) context.getBean("carro");
 		System.out.println(carro.getNome());
 		System.out.println(carro.getValor());
-<<<<<<< HEAD
 
 		SessionFactory sf = new Configuration().configure("src/main/java/br/com/hibernate.cfg.xml")
 				.buildSessionFactory();
-=======
->>>>>>> ffb687d311081f09790cf4316d6afd4fc9bc4b94
+
 	}
 }
