30788c528bb67964343b7700c09592cac0c66dfa
Gabriel Gomes <gabriel_sousa_94@hotmail.com>
2017-10-31 16:42:16 -0200

    Adding Files

---
+++ src/main/java/br/com/App.java
@@ -0,0 +1,23 @@
+package br.com;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+
+		Carro carro = (Carro) context.getBean("carro");
+		System.out.println(carro.getNome());
+		System.out.println(carro.getValor());
+
+		SessionFactory sf = new Configuration().configure("src/main/java/br/com/hibernate.cfg.xml")
+				.buildSessionFactory();
+	}
+}
---
+++ src/main/java/br/com/Carro.java
@@ -0,0 +1,24 @@
+package br.com;
+
+public class Carro {
+
+	private String nome;
+	private float valor;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+}
